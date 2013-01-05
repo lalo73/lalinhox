77a7256537b010d90a4e07ef16ac1aff3b8bbab9
lalo73 <leandro.gomez73@yahoo.com.ar>
2013-01-05 18:49:39 -0300

    Deleted innecesary tools module. Needed implement file system

--- hardware/fileSystem.py
+++ hardware/fileSystem.py
@@ -1,37 +1,23 @@
 __author__ = 'leandro'
 
-from tools import tools
 
 class HardDisk:
     def __init__(self):
         pass
 
 
-class GeneralFileSystem:
+class GeneralFileSystemObject:
     def __init__(self,name):
         self.name = name
 
-class Directory(GeneralFileSystem):
+class Directory(GeneralFileSystemObject):
     def __init__(self,name):
         super(Directory,self).__init__(name)
         self.filesAndDirectories = {}
 
-    def addFile(self,path,file):
-        tools.checkValidPathFormatToDir(path)
-        if path == "/":
-            self.filesAndDirectories[file.name] = file
-        else:
-            directories = path.split("/")
-            directoryName = directories[0]
-            newPath = "/".join(directories[1:])
-            directory = self.getDir(directoryName)
-            directory.addFile(newPath,file)
+    def addFile(self,file,path=""):
+        pass
 
-    def getDir(self,dirName):
-        try:
-            return self.filesAndDirectories[dirName]
-        except KeyError:
-            raise CantFindDirectory()
 
 #Exceptions
 
