744c7ea6cc9a42aefcb762dbe9f828863705ed3e
lgomez <lgomez@devartis.com>
2013-01-11 10:05:14 -0300

    Added mmu interface.

---
+++ hardware/mmu.py
@@ -0,0 +1,17 @@
+__author__ = 'lgomez'
+
+class MMU:
+    def __init__(self):
+        self.__pageTable = PageTable()
+
+    def createPage(self,pcb):
+        pass
+
+    def getMemoryIndexFrom(self,aPCB,pc):
+        pass
+
+    def deletePage(self,aPCB):
+        pass
+
+class PageTable:
+    pass
